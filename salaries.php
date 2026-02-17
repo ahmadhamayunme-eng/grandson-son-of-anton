d9e718491fa5e9e99902c91b05a21640f8ac9558
Ahmad Hamayun <ahmadhamayun.me@gmail.com>
2026-02-17 05:29:07 +0500

    Update salaries.php

--- salaries.php
+++ salaries.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+echo "START\n";
 require_once __DIR__ . '/layout.php';
 require_once __DIR__ . '/lib/activity.php';
 auth_require_perm('finance.view');
